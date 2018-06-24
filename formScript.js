2a8ee3822a7e20206835be06cd248fa88f26a753
VadymBoguslavsky <axixe92@gmail.com>
2018-06-25 02:18:12 +0300

    file input

--- formScript.js
+++ formScript.js
@@ -24,51 +24,28 @@ fetch('http://504080.com/api/v1/directories/enquiry-types')
 		console.log('Fetch Error :-S', err);
 	});
 
-  var input = document.querySelector('#image_uploads');
-  var preview = document.querySelector('.preview');
-  console.log(input)
-  // input.addEventListener('change', updateImageDisplay);function updateImageDisplay() {
-  //   while(preview.firstChild) {
-  //     preview.removeChild(preview.firstChild);
-  //   }
+     window.onload = function() {
+      var fileInput = document.getElementById('image_uploads');
+      var fileDisplayArea =  document.querySelector('.form__image');
+      fileInput.addEventListener('change', function(e) {
+        var file = fileInput.files[0];
+        var imageType = /image.*/;
   
-  //   var curFiles = input.files;
-  //   if(curFiles.length === 0) {
-  //     var para = document.createElement('p');
-  //     para.textContent = 'No files currently selected for upload';
-  //     preview.appendChild(para);
-  //   } else {
-  //     var list = document.createElement('ol');
-  //     preview.appendChild(list);
-  //     for(var i = 0; i < curFiles.length; i++) {
-  //       var listItem = document.createElement('li');
-  //       var para = document.createElement('p');
-  //       if(validFileType(curFiles[i])) {
-  //         para.textContent = 'File name ' + curFiles[i].name + ', file size ' + returnFileSize(curFiles[i].size) + '.';
-  //         var image = document.createElement('img');
-  //         image.src = window.URL.createObjectURL(curFiles[i]);
+        if (file.type.match(imageType)) {
+          var reader = new FileReader();
+          reader.onload = function(e) {
+           
+            var img = new Image();
+            img.src = reader.result;
+            img.classList.add('form-image')
+            fileDisplayArea.appendChild(img);
+          }
+          reader.readAsDataURL(file);	
+        } else {
+          fileDisplayArea.innerHTML = "File not supported!"
+        }
+      });
+    }
   
-  //         listItem.appendChild(image);
-  //         listItem.appendChild(para);
-  
-  //       } else {
-  //         para.textContent = 'File name ' + curFiles[i].name + ': Not a valid file type. Update your selection.';
-  //         listItem.appendChild(para);
-  //       }
-  
-  //       list.appendChild(listItem);
-  //     }
-  //   }
-  // }var fileTypes = [
-  //   'image/jpeg',
-  //   'image/pjpeg',
-  //   'image/png'
-  // ]
-  
-  // function validFileType(file) {
-  //   for(var i = 0; i < fileTypes.length; i++) {
-  //     if(file.type === fileTypes[i]) {
-  //       return true;
-  //     }
-  //   }
-  // }
+      
+    
